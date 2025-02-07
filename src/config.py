800bb5028a185058d67087eacb3d565c0173f22b
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-02-07 16:17:40 +0530

    Setup config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 import os
 
+
 class Config:
+    BASE_URL = os.getenv('BASE_URL', 'http://localhost:5000')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///url_shortener.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
